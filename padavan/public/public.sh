62818a2d9d41bcbbf2f9c4a0c0f60e759e4c3b58
bake1490 <70867426+bake1490@users.noreply.github.com>
2025-02-08 10:20:07 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -115,6 +115,4 @@ echo "CONFIG_FIRMWARE_INCLUDE_DDNS_SSL=y" >>.config
 
 ### Include HTTPS support. openssl ~1.2MB
 echo "CONFIG_FIRMWARE_INCLUDE_HTTPS=y" >>.config
-## 自定义插件
-echo "CONFIG_FIRMWARE_INCLUDE_NGINX=y" >>.config   # nginx ~20M
-echo "CONFIG_FIRMWARE_INCLUDE_DDNSGO=y" >>.config  #DDNS_GO
+
